9cb9ae6b3cdc6219750350e5216ab843cd5f66fd
caracio35 <128204576+caracio35@users.noreply.github.com>
2025-05-25 22:08:32 -0300

    creacion

---
+++ FRONTEND/src/ar/edu/unrn/seminario/main/Main.java
@@ -0,0 +1,28 @@
+package ar.edu.unrn.seminario.main;
+
+import java.awt.EventQueue;
+
+import ar.edu.unrn.seminario.api.IApi;
+import ar.edu.unrn.seminario.api.PersistenceApi;
+import ar.edu.unrn.seminario.gui.VentanaPrincipal;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+
+					IApi api = new PersistenceApi();
+					VentanaPrincipal frame = new VentanaPrincipal(api);
+
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
